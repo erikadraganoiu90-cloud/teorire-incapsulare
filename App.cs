6e58a712d78ef7bc1d90a6aadf010837942719f9
Erika <erikadraganoiu90@gmail.com>
2026-09-22 19:19:07 +0300

    first commit

---
+++ App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teorie_incapsulare
+{
+    internal class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ce mai faci");
+        }
+    }
+}
